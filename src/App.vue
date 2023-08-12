3b96406b1412bc8cfc4966387b09cdd0f6e6f76e
tedabytez <tdansa64@example.com>
2023-08-12 18:19:16 +0100

    ground zero

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <header>
+      <h1>Quizes</h1>
+      <input type="text">
+    </header>
+  <div class="cards-container">
+
+  </div>
+  </div>
+</template>
+
+<style scoped>
+.container{
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+header{
+  margin-top: 30px;
+  margin-bottom: 10px;
+  display: flex;
+  align-items: center;
+}
+
+header h1 {
+  font-weight: bold;
+  margin-right: 30px;
+}
+
+header input{
+  border: none;
+  background-color: rgba(255, 255, 255, 0.737);
+  border-radius: 5px;
+  padding: 5px;
+}
+</style>
