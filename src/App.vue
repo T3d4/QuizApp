0e66a07d9dfc40dfaf7d29a2738209e8d5c443bb
tedabytez <tdansa64@gmail.com>
2023-08-18 10:22:20 +0100

    minor changes

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,16 @@
 <script setup>
-import qiz from './data/quizes.json';
-import {ref, watch} from 'vue';
-import Card from './components/Card.vue';
-
-const quizes = ref(qiz);
-const searchInput = ref('');
-
-watch(searchInput, ()=>
-  quizes.value = qiz.filter(quiz => quiz.name.toLowerCase().includes(searchInput.value.toLowerCase()))
-)
+import { RouterView } from "vue-router";
 </script>
 
 <template>
   <div class="container">
-    <header>
-      <h1>Quizes</h1>
-      <input v-model.trim="searchInput" type="text" placeholder="search...">
-    </header>
-  <div class="cards-container">
-    <Card  v-for="quiz in quizes" :key="quiz.id" :quiz="quiz"/>
-  </div>
+    <RouterView />
   </div>
 </template>
 
 <style scoped>
-.container{
+.container {
   max-width: 1000px;
   margin: 0 auto;
 }
-
-header{
-  margin-top: 30px;
-  margin-bottom: 10px;
-  display: flex;
-  align-items: center;
-}
-
-header h1 {
-  font-weight: bold;
-  margin-right: 30px;
-}
-
-header input{
-  border: none;
-  background-color: rgba(255, 255, 255, 0.737);
-  border-radius: 5px;
-  padding: 5px;
-}
-
-.cards-container{
-  display: flex;
-  flex-wrap: wrap;
-  margin-top: 30px;
-}
-
-/* CARD */
-
 </style>
